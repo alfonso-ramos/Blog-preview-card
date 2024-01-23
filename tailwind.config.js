b078843327b7bce9784aaa813d53f74d9b7de038
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-01-23 07:27:30 -0700

    [ADD] Maquetacion inicial del componente, piliendo estilos

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'yellow': 'hsl(47, 88%, 63%)',
+      'black': '#000',
+      'white': '#fff',
+      'gray': '#7F7F7F'
+    }
+  },
+  plugins: [],
+}
